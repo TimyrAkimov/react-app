bb2400d880bc050a1468180b9b7b7773209d2685
TimyrAkimov <timyrakimov03@gmail.com>
2023-01-24 02:40:31 +0300

    create-react-app

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: { 
+      colors: {
+      "lavender": "#7E3AF2",
+      "white": "#FCFBFA",
+      "black": "#27272A",
+      "grey": "#E5E7EB"
+    },
+  },
+  },
+  plugins: [],
+}
